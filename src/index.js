3aebb522d4cf6b2d78073a91d707d8e80e579872
Uros Ivanovic <urosivanovic@Uross-MBP-124.lan>
2021-11-29 19:45:17 -0500

    all done

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ const contact = document.querySelector('section.contact')
 
 contact.querySelector('h4').textContent = siteContent.contact['contact-h4']
 contact.children[1].textContent = siteContent.contact['address']
-contact.querySelector('p:nth-og-type(2)').textContent = siteContent.contact['phone']
+contact.querySelector('p:nth-of-type(2)').textContent = siteContent.contact['phone']
 contact.children[3].textContent = siteContent.contact['email']
 
 
